ac71651b6a26fa1ed747576359a7d61b9d0e74f5
Adam Jakab <adam@jakab.pro>
2022-06-04 23:21:43 +0200

    inline-4

--- integration/javascript/inline/index.php
+++ integration/javascript/inline/index.php
@@ -3,9 +3,8 @@
 /** @var $qit_config [] */
 require_once(__DIR__ . '/../../../config.php');
 
-$qit_queueclient_js = file_get_contents(__DIR__ . '/js/queueclient.min.js');
-$qit_queueconfigloader_js = file_get_contents(__DIR__ . '/js/queueconfigloader.min.js');
-
+$qit_queueclient_js = file_get_contents(__DIR__ . '/js/queueclient.js');
+$qit_queueconfigloader_js = file_get_contents(__DIR__ . '/js/queueconfigloader.js');
 
 
 ?>
